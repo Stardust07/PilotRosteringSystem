0b0d3ac1ab67fa70586510b9e1403ef531ad4860
Stardust07 <yanminqi@hustunique.com>
2016-04-08 22:12:24 +0800

    初始排班表

--- PilotRosteringSystem/Form1.Designer.cs
+++ PilotRosteringSystem/Form1.Designer.cs
@@ -29,29 +29,104 @@
         private void InitializeComponent()
         {
             this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.previousButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pageBox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView
             // 
             this.dataGridView.AllowUserToAddRows = false;
             this.dataGridView.AllowUserToDeleteRows = false;
-            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView.Location = new System.Drawing.Point(12, 54);
+            this.dataGridView.AllowUserToResizeColumns = false;
+            this.dataGridView.AllowUserToResizeRows = false;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.ColumnHeadersHeight = 45;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView.EnableHeadersVisualStyles = false;
+            this.dataGridView.Location = new System.Drawing.Point(13, 54);
             this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersVisible = false;
             this.dataGridView.RowTemplate.Height = 27;
-            this.dataGridView.Size = new System.Drawing.Size(982, 501);
+            this.dataGridView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView.Size = new System.Drawing.Size(982, 533);
             this.dataGridView.TabIndex = 0;
+            this.dataGridView.VirtualMode = true;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(13, 23);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(200, 25);
+            this.dateTimePicker.TabIndex = 1;
+            this.dateTimePicker.ValueChanged += new System.EventHandler(this.dateTimePicker_ValueChanged);
+            // 
+            // previousButton
+            // 
+            this.previousButton.Location = new System.Drawing.Point(12, 3);
+            this.previousButton.Name = "previousButton";
+            this.previousButton.Size = new System.Drawing.Size(75, 26);
+            this.previousButton.TabIndex = 2;
+            this.previousButton.Text = "上一页";
+            this.previousButton.UseVisualStyleBackColor = true;
+            this.previousButton.Click += new System.EventHandler(this.previousButton_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(137, 3);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 26);
+            this.nextButton.TabIndex = 3;
+            this.nextButton.Text = "下一页";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(246, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "共 页";
+            // 
+            // pageBox
+            // 
+            this.pageBox.Location = new System.Drawing.Point(93, 3);
+            this.pageBox.Name = "pageBox";
+            this.pageBox.Size = new System.Drawing.Size(38, 25);
+            this.pageBox.TabIndex = 5;
+            this.pageBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.nextButton);
+            this.panel1.Controls.Add(this.pageBox);
+            this.panel1.Controls.Add(this.previousButton);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(700, 668);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(294, 41);
+            this.panel1.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1006, 721);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dateTimePicker);
             this.Controls.Add(this.dataGridView);
             this.Name = "Form1";
             this.Text = "飞行员训练排班系统";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -59,6 +134,12 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Button previousButton;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pageBox;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- PilotRosteringSystem/Form1.cs
+++ PilotRosteringSystem/Form1.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,10 +15,184 @@ namespace PilotRosteringSystem
 {
     public partial class Form1 : Form
     {
+        [DllImport("PilotRosteringSolver.dll", EntryPoint = "EC_OpenPort", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern uint EC_OpenPort(String port, String param);
 
+
+        [DllImport("PilotRosteringSolver.dll", EntryPoint = "EC_ClosePort", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void EC_ClosePort(uint hdl_port);
+
+
+        [DllImport("PilotRosteringSolver.dll", EntryPoint = "EC_RunCmd", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern bool EC_RunCmd(uint hdl_port, byte[] cmd);
+
+        private const int PER_PAGE = 7;
+        private const String DATE = "date";
+        private DataTable sourceData;
+        private int currentPage = 1;
+        private int year;
+        private int pageCount = 0;
         public Form1()
         {
             InitializeComponent();
+            String fileName = "roster.csv";
+            dataGridView.ColumnHeadersDefaultCellStyle.BackColor = Color.Gray;
+            loadRoster(fileName);
         }
+        private void loadRoster(String fileName)
+        {
+            sourceData = getData(fileName);
+            int day = dateTimePicker.Value.DayOfYear;
+            currentPage = (day - 1) / 7 + 1;
+            dataGridView.DataSource = getDataByPage(currentPage);
+
+            //dataGridView.Rows[0].Cells[0].Selected = false;
+            //int rowCount = dataGridView.Rows.Count;
+            //int currentIndex = day - (currentPage - 1) * 7 - 1;
+            //for (int i = 0; i < rowCount; i++)
+            //{
+            //    dataGridView.Rows[i].Cells[currentIndex].Selected = true;
+            //}
+
+            label1.Text = "共" + pageCount.ToString() + "页";
+            setPage();
+        }
+        private void setPage()
+        {
+            pageBox.Text = currentPage.ToString();
+            if (currentPage == 1)
+            {
+                previousButton.Enabled = false;
+                nextButton.Enabled = true;
+            }
+            else if (currentPage == pageCount)
+            {
+                previousButton.Enabled = true;
+                nextButton.Enabled = false;
+            }
+            else
+            {
+                previousButton.Enabled = true;
+                nextButton.Enabled = true;
+            }
+        }
+        private DataTable getData(String filePath)
+        {
+            Encoding encoding = Encoding.ASCII;
+            DataTable dataTable = new DataTable();
+            GregorianCalendar gc = new GregorianCalendar();
+            DateTime dt;
+            int spareDay;
+
+            FileStream fs = new FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+
+            StreamReader sr = new StreamReader(fs, encoding);
+            string strLine = "";
+            //记录每行记录中的各字段内容
+            string[] aryLine = null;
+            string[] tableHead = null;
+            int columnCount = 0;
+            //读取表头
+            if ((strLine = sr.ReadLine()) != null)
+            {
+                DataColumn dataColumn = new DataColumn(DATE);
+                dataTable.Columns.Add(dataColumn);
+                tableHead = strLine.Split(',');
+                columnCount = tableHead.Length - 1;
+                year = Convert.ToInt32(tableHead[1].Substring(0, 4));
+                dt = gc.ToDateTime(year, 1, 1, 0, 0, 0, 0);
+                spareDay = Convert.ToInt32(dt.DayOfWeek) - 1;
+
+                for (int i = spareDay - 1; i >= 0 ; i--)
+                {
+                    dataColumn = new DataColumn((year - 1).ToString() + "12" + (31 - i).ToString());
+                    dataTable.Columns.Add(dataColumn);
+                }
+                for (int i = 1; i <= columnCount; i++)
+                {
+                    dataColumn = new DataColumn(tableHead[i]);
+                    dataTable.Columns.Add(dataColumn);
+                }
+            }
+            else
+            {
+                return null;
+            }
+            while ((strLine = sr.ReadLine()) != null)
+            {
+                aryLine = strLine.Split(',');
+                DataRow dataRow = dataTable.NewRow();
+                dataRow[0] = aryLine[0];
+                for (int j = 1; j < columnCount; j++)
+                {
+                    dataRow[j + spareDay] = aryLine[j];
+                }
+                dataTable.Rows.Add(dataRow);
+            }
+
+            sr.Close();
+            fs.Close();
+            pageCount = (dataTable.Columns.Count - 2) / PER_PAGE + 1;
+            return dataTable;
+        }
+        private DataTable getDataByPage(int pageIndex)
+        {
+            pageIndex--;
+            DataTable desData = new DataTable();
+            int columnsCount = sourceData.Columns.Count;
+            desData.Columns.Add(new DataColumn(DATE));
+            for (int i = 0; i < PER_PAGE && i + pageIndex * 7 + 1 < columnsCount; i++)
+            {
+                DataColumn dataColumn = new DataColumn(sourceData.Columns[i + pageIndex * 7 + 1].ColumnName);
+                desData.Columns.Add(dataColumn);
+            }
+            for (int i = 0; i + desData.Columns.Count <= PER_PAGE; i++)
+            {
+                DataColumn dataColumn = new DataColumn((year + 1).ToString() + "010" + (1 + i).ToString());
+                desData.Columns.Add(dataColumn);
+            }
+            int rowsCount = sourceData.Rows.Count;
+            for (int i = 0; i < rowsCount; i++)
+            {
+                DataRow dataRow = desData.NewRow();
+                dataRow[0] = sourceData.Rows[i][0].ToString();
+                for (int j = 1; j <= PER_PAGE && j + pageIndex * 7 < columnsCount; j++)
+                {
+                    dataRow[j] = sourceData.Rows[i][j + pageIndex * 7].ToString();
+                }
+                desData.Rows.Add(dataRow);
+
+            }
+
+            return desData;
+        }
+
+
+        private void dateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            int futurePage = (dateTimePicker.Value.DayOfYear - 1) / 7 + 1;
+            if (currentPage == futurePage)
+            {
+                return;
+            }
+            currentPage = futurePage;
+            dataGridView.DataSource = getDataByPage(currentPage);
+            setPage();
+        }
+
+
+        private void previousButton_Click(object sender, EventArgs e)
+        {
+            dataGridView.DataSource = getDataByPage(--currentPage);
+            setPage();
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            dataGridView.DataSource = getDataByPage(++currentPage);
+            setPage();
+        
+        }
+
     }
 }
