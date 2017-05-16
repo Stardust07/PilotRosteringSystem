64f61275539768062326f77b34fd3bd42407360e
QiYanmin <yanminqi@hustunique.com>
2017-05-16 09:39:20 +0800

    输入模块修改

--- PilotRosteringSystem/SelectSubjects.Designer.cs
+++ PilotRosteringSystem/SelectSubjects.Designer.cs
@@ -30,6 +30,12 @@
         {
             this.checkedListBox = new System.Windows.Forms.CheckedListBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.subjectGrid = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.subjectGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // checkedListBox
@@ -38,33 +44,85 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.checkedListBox.CheckOnClick = true;
             this.checkedListBox.FormattingEnabled = true;
-            this.checkedListBox.Location = new System.Drawing.Point(12, 12);
+            this.checkedListBox.Location = new System.Drawing.Point(9, 10);
+            this.checkedListBox.Margin = new System.Windows.Forms.Padding(2);
             this.checkedListBox.Name = "checkedListBox";
-            this.checkedListBox.Size = new System.Drawing.Size(305, 204);
+            this.checkedListBox.Size = new System.Drawing.Size(14, 164);
             this.checkedListBox.TabIndex = 1;
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.AutoSize = true;
-            this.button1.Location = new System.Drawing.Point(242, 313);
-            this.button1.Margin = new System.Windows.Forms.Padding(5);
+            this.button1.Location = new System.Drawing.Point(182, 248);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.Size = new System.Drawing.Size(56, 22);
             this.button1.TabIndex = 2;
             this.button1.Text = "确定";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // subjectGrid
+            // 
+            this.subjectGrid.AllowUserToAddRows = false;
+            this.subjectGrid.AllowUserToDeleteRows = false;
+            this.subjectGrid.AllowUserToResizeRows = false;
+            this.subjectGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.subjectGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.subjectGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.subjectGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.subjectGrid.Location = new System.Drawing.Point(28, 10);
+            this.subjectGrid.MultiSelect = false;
+            this.subjectGrid.Name = "subjectGrid";
+            this.subjectGrid.RowHeadersVisible = false;
+            this.subjectGrid.RowTemplate.Height = 23;
+            this.subjectGrid.Size = new System.Drawing.Size(210, 164);
+            this.subjectGrid.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 5;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "科目编号";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 61;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "白天训练次数";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 72;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "夜间训练次数";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 72;
+            // 
             // SelectSubjects
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(329, 352);
+            this.ClientSize = new System.Drawing.Size(247, 282);
+            this.Controls.Add(this.subjectGrid);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkedListBox);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "SelectSubjects";
             this.Text = "选择科目";
+            ((System.ComponentModel.ISupportInitialize)(this.subjectGrid)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -74,6 +132,11 @@
 
         private System.Windows.Forms.CheckedListBox checkedListBox;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView subjectGrid;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
 
     }
 }
--- PilotRosteringSystem/SelectSubjects.cs
+++ PilotRosteringSystem/SelectSubjects.cs
@@ -56,65 +56,74 @@ namespace PilotRosteringSystem
                 cellContent = "";
             }
             InitializeComponent();
+            
             for (int i = 0; i < subjectTable.RowCount - 1; i++)
             {
                 DataGridViewRow row = subjectTable.Rows[i];
                 checkedListBox.Items.Add(row.Cells[0].Value.ToString());
+
+                DataGridViewRow newRow = new DataGridViewRow();
+                DataGridViewCheckBoxCell checkboxCell = new DataGridViewCheckBoxCell();
+                checkboxCell.Value = false;
                 if (cellContent.Contains(row.Cells[0].Value.ToString()))
                 {
-                    checkedListBox.SetItemChecked(i, true); 
+                    checkedListBox.SetItemChecked(i, true);
+                    checkboxCell.Value = true;
                 }
+                newRow.Cells.Add(checkboxCell);
+                DataGridViewTextBoxCell textCell = new DataGridViewTextBoxCell();
+                textCell.Value = row.Cells[0].Value;
+                newRow.Cells.Add(textCell);
+                textCell = new DataGridViewTextBoxCell();
+                newRow.Cells.Add(textCell);
+                textCell = new DataGridViewTextBoxCell();
+                newRow.Cells.Add(textCell);
+                subjectGrid.Rows.Add(newRow);
             }
         }
         
         private void button1_Click(object sender, EventArgs e)
         {
             String str = "";
+            int checkedCount = 0;
 
-            if (checkedListBox.CheckedItems.Count <= 0)
+            for (int i = 0; i < subjectGrid.Rows.Count - 1; i++)
             {
-                ;
-            }
-            else
-            {
-                for (int i = 0; i < checkedListBox.CheckedItems.Count - 1; i++)
+                DataGridViewRow dataRow = subjectGrid.Rows[i];
+                if (dataRow.Cells[0].Value.Equals(true))
                 {
-                    str += checkedListBox.CheckedItems[i].ToString() + " ";
+                    checkedCount++;
+                    str += dataRow.Cells[1].Value.ToString() + " ";
                 }
-                str += checkedListBox.CheckedItems[checkedListBox.CheckedItems.Count - 1].ToString();
             }
+            if (subjectGrid.Rows[subjectGrid.Rows.Count - 1].Cells[0].Value.Equals(true))
+            {
+                checkedCount++;
+                str += subjectGrid.Rows[subjectGrid.Rows.Count - 1].Cells[1].Value.ToString();
+            }
+
+            //if (checkedListBox.CheckedItems.Count <= 0)
+            //{
+            //    ;
+            //}
+            //else
+            //{
+            //    for (int i = 0; i < checkedListBox.CheckedItems.Count - 1; i++)
+            //    {
+            //        str += checkedListBox.CheckedItems[i].ToString() + " ";
+            //    }
+            //    str += checkedListBox.CheckedItems[checkedListBox.CheckedItems.Count - 1].ToString();
+            //}
             
             if (pilotTable == null)
             {
-                subjectTable.Rows[selectedRow].Cells[5].Value = checkedListBox.CheckedItems.Count;
+                subjectTable.Rows[selectedRow].Cells[5].Value = checkedCount;
                 subjectTable.Rows[selectedRow].Cells[6].Value = str;
-                //if (subjectTable.SelectedRows.Count == 0)
-                //{
-                //    int rowIndex = subjectTable.SelectedCells[0].RowIndex;
-                //    subjectTable.Rows[rowIndex].Cells[5].Value = checkedListBox.CheckedItems.Count;
-                //    subjectTable.Rows[rowIndex].Cells[6].Value = str;
-                //}
-                //else
-                //{
-                //    subjectTable.SelectedRows[0].Cells[5].Value = checkedListBox.CheckedItems.Count;
-                //    subjectTable.SelectedRows[0].Cells[6].Value = str;
-                //}    
             }
             else
             {
-                pilotTable.Rows[selectedRow].Cells[2].Value = checkedListBox.CheckedItems.Count;
-                pilotTable.Rows[selectedRow].Cells[3].Value = str;
-                //if (pilotTable.SelectedRows.Count == 0)
-                //{
-                //    int rowIndex = pilotTable.SelectedCells[0].RowIndex;
-                //    pilotTable.Rows[rowIndex].Cells[2].Value = checkedListBox.CheckedItems.Count;
-                //    pilotTable.Rows[rowIndex].Cells[3].Value = str;
-                //}
-                //else
-                //{
-                //    pilotTable.SelectedRows[0].Cells[2].Value = checkedListBox.CheckedItems.Count;
-                //    pilotTable.SelectedRows[0].Cells[3].Value = str;
-                //}    
+                pilotTable.Rows[selectedRow].Cells[2].Value = checkedCount;
+                pilotTable.Rows[selectedRow].Cells[3].Value = str;  
             }
             this.Close();
         }
